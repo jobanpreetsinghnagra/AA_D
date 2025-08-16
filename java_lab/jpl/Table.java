af5adbb50593e06ef7dc13b90cbbdcd6857c9643
joban <130277649+jobanpreetsinghnagra@users.noreply.github.com>
2025-08-16 19:27:05 +0530

    Add files via upload

---
+++ java_lab/jpl/Table.java
@@ -0,0 +1,13 @@
+class Table
+{
+    public static void main(String[] args)
+    {
+        int table_n = 4;
+        int sum =0;
+        for(int i  =0;i<=9;i++)
+        {
+            sum = sum +table_n;
+            System.out.println("The sum is: "+sum);
+        }
+    }
+}

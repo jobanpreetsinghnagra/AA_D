fd08992c5d243f4f72e3c15ca0d929ac94f086bc
joban <nagraboy01@gmail.com>
2025-08-28 10:36:01 +0530

    string programs

---
+++ java_lab/jpl/strings/LCase.java
@@ -0,0 +1,9 @@
+public class LCase{
+    public static void main(String[] args) {
+        String input = "Hello WORLD! This is a TEST String.";
+        String result = input.toLowerCase();
+
+        System.out.println("Original: " + input);
+        System.out.println("Lowercase: " + result);
+    }
+}
